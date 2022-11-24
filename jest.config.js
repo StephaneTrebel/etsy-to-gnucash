e8eb62798a740f79ac3a04d5ff247e3552ebd9f3
Stéphane TREBEL <stephane.trebel@gmail.com>
2022-11-24 20:51:25 +0100

    feat(tests) Add coverage

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+
   moduleNameMapper: {
     '^csv-stringify/sync':
       '<rootDir>/node_modules/csv-stringify/dist/cjs/sync.cjs',
     '^csv-parse/sync': '<rootDir>/node_modules/csv-parse/dist/cjs/sync.cjs',
   },
+
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.ts', '!**/*.spec.ts', '!**/node_modules/**'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'json', ['text', { skipFull: true }]],
 };
